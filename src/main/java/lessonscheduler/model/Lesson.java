4e0aa785e2e148c8cfbbe29c971c822b8de79775
IT24101566 <IT24101566@my.sliit.lk>
2025-05-18 19:09:31 +0530

    Update Lesson.java

--- src/main/java/lessonscheduler/model/Lesson.java
+++ src/main/java/lessonscheduler/model/Lesson.java
@@ -1,6 +1,5 @@
 package com.dulara.lessonscheduler.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
 import java.time.LocalDateTime;
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
