8be9f82a049c7ab51cafacd5aacf2944ff99cc92
IT24300049 <it24300049@my.sliit.lk>
2025-05-19 21:12:13 +0530

    Delete src directory

--- src/main/java/com/vehiclehandling/AutomaticVehicle.java
+++
@@ -1,8 +0,0 @@
-package com.vehiclehandling;
-
-public class AutomaticVehicle extends Vehicle {
-    public AutomaticVehicle(int id, String make, String model, String registrationNumber, String maintenanceSchedule,
-                            String color, int year) {
-        super(id, make, model, registrationNumber, maintenanceSchedule, color, year, "Automatic");
-    }
-}
--- src/main/java/com/vehiclehandling/ManualVehicle.java
+++
@@ -1,8 +0,0 @@
-package com.vehiclehandling;
-
-public class ManualVehicle extends Vehicle {
-    public ManualVehicle(int id, String make, String model, String registrationNumber, String maintenanceSchedule,
-                         String color, int year) {
-        super(id, make, model, registrationNumber, maintenanceSchedule, color, year, "Manual");
-    }
-}
