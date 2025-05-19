cbd4692cdbb2b496eefc08c64371bedc4b717a87
Yasi24Hase <IT24101654@my.sliit.lk>
2025-05-19 20:01:44 +0530

    Getter & Setter

--- src/main/java/com/example/driving_school/entity/Payment.java
+++ src/main/java/com/example/driving_school/entity/Payment.java
@@ -6,8 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Payment {
 
     @JsonProperty("paymentId")
@@ -27,4 +25,54 @@ public class Payment {
 
     @JsonProperty("paymentStatus")
     private String paymentStatus;
+
+    public Payment(){}
+
+    public Payment(String paymentId, String studentId, String lessonId, String paymentDate, double amount, String paymentStatus) {
+        this.paymentId = paymentId;
+        this.studentId = studentId;
+        this.lessonId = lessonId;
+        this.paymentDate = paymentDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getLessonId() {
+        return lessonId;
+    }
+    public void setLessonId(String lessonId) {
+        this.lessonId = lessonId;
+    }
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+
 }
